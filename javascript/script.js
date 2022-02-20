522a227489e9ee134f8be4e012656f2f96726bb8
NishantKumarTiwari <nishant@italentindia.com>
2022-02-20 00:24:30 -0800

    made productlisting responsive

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+const filterShow = document.querySelector(".filter-sidebar")
+
+
+
+
+const openFilter=()=>{
+    filterShow.style.display = "block"
+}
+const closeFilter=()=>{
+    filterShow.style.display = "none"
+}
